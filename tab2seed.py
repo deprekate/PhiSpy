8d378fefe07112465d2cf9c3b3d8a62440279dea
jfsanchez <jfbioinformatics@gmail.com>
2019-06-13 11:01:07 +0200

    Copyrigth and clarifying

--- tab2seed.py
+++ tab2seed.py
@@ -1,3 +1,43 @@
+#!/usr/bin/env python
+#############################################################################################
+## PhiSpy is a computer program written in C++, Python and R to 
+## identify prophages in a complete bacterial genome sequences.
+##
+## Initial versions of PhiSpy were written by
+## Sajia Akhter (sajia@stanford.edu) PhD Student Edwards Bioinformatics Lab 
+## (http://edwards.sdsu.edu/labsite/), Computational Science Research Center 
+## (http://www.csrc.sdsu.edu/csrc/), San Diego State University (http://www.sdsu.edu/)
+##
+## Improvements, bug fixes, and other changes were made by
+## Katelyn McNair Edwards Bioinformatics Lab (http://edwards.sdsu.edu/labsite/) 
+## San Diego State University (http://www.sdsu.edu/)
+## 
+## The MIT License (MIT)
+## Copyright (c) 2016 Rob Edwards
+## Permission is hereby granted, free of charge, to any person obtaining a copy
+## of this software and associated documentation files (the "Software"), to deal
+## in the Software without restriction, including without limitation the rights
+## to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+## copies of the Software, and to permit persons to whom the Software is
+## furnished to do so, subject to the following conditions:
+## 
+## The above copyright notice and this permission notice shall be included in all
+## copies or substantial portions of the Software.
+## 
+## THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+## IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+## FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+## AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+## LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+## OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+## SOFTWARE.
+## 
+## Improvements, bug fixes and other changes (2019) were made by forking the github version: 
+## - Conversion from python2 -> python3
+## - Add prophage.tbl
+## - Simplification of code and integration into BacterialGenotyper
+##
+#############################################################################################
 """
 Convert a tab file from PATRIC to a the SEED files that we need for PhiSpy
 
@@ -26,6 +66,7 @@ import os
 import sys
 import argparse
 
+#################################################
 def parse_tab(filename, outputdir):
     """
     Parse a patric tab file
@@ -76,8 +117,7 @@ def parse_tab(filename, outputdir):
     rna.close()
     asf.close()
 
-
-
+#################################################
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Convert a patric tab file to a minimal seed directory")
     parser.add_argument('-f', help='The patric tab file', required=True)
